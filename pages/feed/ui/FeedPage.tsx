0d950bafc33807cc910ae383d0f972d832ff3660
Lev Chelyadinov <leva181777@gmail.com>
2024-02-03 15:33:18 +0100

    Connect the feed page

---
+++ pages/feed/ui/FeedPage.tsx
@@ -0,0 +1,12 @@
+export function FeedPage() {
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1 className="logo-font">conduit</h1>
+          <p>A place to share your knowledge.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
