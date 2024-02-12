b922153055cc764c53cd28eb75e8b93c0dcb2ca5
Lev Chelyadinov <leva181777@gmail.com>
2024-02-12 19:19:48 +0100

    Add hot reloading to other layer folders

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  watchPaths: ["shared", "pages"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
