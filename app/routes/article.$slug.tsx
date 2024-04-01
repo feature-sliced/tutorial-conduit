478a9e57f89d2ecaa56f9b95f17578743f47797d
Lev Chelyadinov <leva181777@gmail.com>
2024-04-01 13:31:55 +0200

    Implement the article reader

---
+++ app/routes/article.$slug.tsx
@@ -0,0 +1,5 @@
+import { ArticleReadPage } from "pages/article-read";
+
+export { loader, action } from "pages/article-read";
+
+export default ArticleReadPage;
---
+++ pages/article-read/index.ts
@@ -0,0 +1,3 @@
+export { ArticleReadPage } from "./ui/ArticleReadPage";
+export { loader } from "./api/loader";
+export { action } from "./api/action";
